957faa68777ec2d513af495ca4c69b793e372565
Your Name <742862703@qq.com>
2018-02-24 05:34:24 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //
     public function show(User $user)
     {
@@ -17,11 +22,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+    	//update 是指授权类里的 update 授权方法，$user 对应传参 update 授权方法的第二个参数。调用时，默认情况下，我们 不需要 传递第一个参数，也就是当前登录用户至该方法内，因为框架会 自动 加载当前登录用户
+    	$this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
