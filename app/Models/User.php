4ed03b02c0efc59b01d50129a0ece64455a2ed8d
Your Name <742862703@qq.com>
2018-02-27 00:47:01 +0000

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    // 默认的 User 模型中使用了 trait —— Notifiable，它包含着一个可以用来发通知的方法 notify() ，此方法接收一个通知实例做参数
+    use Notifiable{
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -41,5 +45,23 @@ class User extends Authenticatable
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+    //对notify方法进行定制，重写notify方法
+     public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        //调用自带通知
+        $this->laravelNotify($instance);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
---
+++ resources/views/notifications/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title')
+我的通知 
+@stop
+
+@section('content')
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+
+                <div class="panel-body">
+
+                    <h3 class="text-center">
+                        <span class="glyphicon glyphicon-bell" aria-hidden="true"></span> 我的通知
+                    </h3>
+                    <hr>
+                    <!-- App\Notifications\TopicReplied 。 snake_case(class_basename($notification->type)) 渲染以后会是 —— topic_replied。class_basename() 方法会取到 TopicReplied，Laravel 的辅助方法 snake_case() 会字符串格式化为下划线命名 -->
+                    @if ($notifications->count())    
+                        <div class="notification-list">
+                            @foreach ($notifications as $notification)
+                                @include('notifications.types._' . snake_case(class_basename($notification->type)))
+                            @endforeach
+
+                            {!! $notifications->render() !!}
+                        </div>
+
+                    @else
+                        <div class="empty-block">没有消息通知！</div>
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
